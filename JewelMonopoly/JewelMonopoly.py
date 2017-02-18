35193ac49503f2b11c4fc414965d3db0e7ec2f5b
Brian Randle <Brian Randle>
2017-02-18 00:17:22 -0600

    Using JSON to Store the prizes, change tickets to lowercase, validate ticket etered

--- JewelMonopoly/JewelMonopoly.py
+++ JewelMonopoly/JewelMonopoly.py
@@ -13,16 +13,43 @@ check all values
 
 
 '''
+import os
+import json
+from pprint import pprint
+
+def getPrizes():
+    print("the cwd is " + os.getcwd())
+    with open(os.getcwd() + "/prizes.json", 'r') as f:     
+        prizes = json.load(f)
+    return prizes
+
+def getPrizeIndex(t, prizes):
+    for idx, row in enumerate(prizes['prizes']): 
+        #print(row['prize'])
+        if row['prize'] == t:
+            return idx
+    return None
+
+prizes = getPrizes()
+pprint(prizes)
 
 # get the current path?
-filename = "C:\python\Monopoly\monopoly_tickets.txt"
-
-print ("We're going to use %s" % filename)
-
+filename = os.getcwd() + "\monopoly_tickets.txt"
 
 target = open(filename, 'a')
 
-ticket_num = input("Enter ticket number (2 left and 1 righmost characters): ")
+ticket_in = input("Enter ticket number (2 left and 1 righmost characters): ")
+ticket_prize = ticket_in[:2].lower()
+prize_ticket_index = getPrizeIndex(ticket_prize, prizes)
+if prize_ticket_index == None:
+    print("Umm, I can't find a prize for " + ticket_prize)
+    exit()
+ticket_num = ticket_in[:-1].lower()
+print("the tickets needed are:")
+print(prizes['prizes'][prize_ticket_index]['tickets'])
+print('And you win '+ prizes['prizes'][prize_ticket_index]['win'])
 
-target.write("\n" + ticket_num)  # CHanged this to have the newline character before the ticket number
+
+target.write("\n" + ticket_prize + ticket_in[:-1].lower())  # CHanged this to have the newline character before the ticket number
+
 
